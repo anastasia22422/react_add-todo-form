20acd2b6f82fc4d86a44aec268eaa164db35bd79
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-07-12 19:29:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,125 @@
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { useState } from 'react';
+
+import { TodoList } from './components/TodoList';
+import { Todo } from './entities';
+
+const getTodos: Todo[] = todosFromServer.map(todo => {
+  const todosUser = usersFromServer.find(user => user.id === todo.userId);
+
+  return { user: todosUser, ...todo };
+});
 
 export const App = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([...getTodos]);
+
+  const [currentUserId, setCurrentUserId] = useState(0);
+  const [textField, setTextField] = useState('');
+
+  const [isTextFieldValid, setIsTextFieldValid] = useState(false);
+  const [isUserOptionValid, setIsUserOptionValid] = useState(false);
+
+  const isNotUser = currentUserId === 0;
+  const isTextFieldEmpty = textField === '';
+
+  const handleTitlePrepare = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const rawText = event.target.value.trimStart();
+
+    const allowedCharsPattern =
+      // eslint-disable-next-line max-len
+      /[^a-zA-Z0-9іІїЇєЄґҐабвгдезжзийклмнопрстуфхцчшщьюяАБВГҐДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЮЯ\s]/g;
+
+    const cleanedText = rawText.replace(allowedCharsPattern, '');
+
+    setTextField(cleanedText);
+    setIsTextFieldValid(false);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (isTextFieldEmpty) {
+      setIsTextFieldValid(true);
+    }
+
+    if (isNotUser) {
+      setIsUserOptionValid(true);
+    }
+
+    if (isNotUser || isTextFieldEmpty) {
+      return;
+    }
+
+    const currenIdsArr = todoList.map(todo => todo.id);
+    const newId = (currenIdsArr.length ? Math.max(...currenIdsArr) : 0) + 1;
+    const currentUser = usersFromServer.find(user => user.id === currentUserId);
+
+    const newTodo: Todo = {
+      user: currentUser,
+      id: newId,
+      title: textField,
+      completed: false,
+      userId: currentUser?.id ?? 0,
+    };
+
+    setTodoList(currentTodos => [...currentTodos, newTodo]);
+    setIsTextFieldValid(false);
+    setIsUserOptionValid(false);
+    setTextField('');
+    setCurrentUserId(0);
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
+      <form
+        action="/api/todos"
+        method="POST"
+        onSubmit={event => handleSubmit(event)}
+      >
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <input
+            type="text"
+            data-cy="titleInput"
+            placeholder="Enter a title"
+            value={textField}
+            onChange={event => handleTitlePrepare(event)}
+          />
+
+          {isTextFieldEmpty && isTextFieldValid && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <select
+            data-cy="userSelect"
+            value={currentUserId}
+            onChange={event => {
+              setCurrentUserId(+event.target.value);
+              setIsUserOptionValid(false);
+            }}
+          >
             <option value="0" disabled>
               Choose a user
             </option>
+
+            {usersFromServer.map(user => {
+              return (
+                <option key={user.id} value={user.id}>
+                  {user.name}
+                </option>
+              );
+            })}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {isNotUser && isUserOptionValid && (
+            <span className="error">Please choose a user</span>
+          )}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -29,33 +127,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todoList} />
     </div>
   );
 };
